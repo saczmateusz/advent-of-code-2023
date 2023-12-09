6341921d14a8eff62135ef593910cc715a04cc2e
Mateusz Sączawa <sacz.mateusz@gmail.com>
2023-12-09 17:27:25 +0100

    day01

---
+++ README.md
@@ -0,0 +1,21 @@
+# [🎄 Advent of Code 🎄](https://adventofcode.com/)
+
+Advent of Code 2023 Solutions in Javascript using Node.js
+
+## :santa: Prerequisites
+
+You need to have Node.js & npm installed locally. Personally, I recommend nvm Node version manager, available [here](https://github.com/coreybutler/nvm-windows).
+
+## :snowman: Getting Started
+
+Clone repository and go to `dayXX` folder.
+
+Provide your input to `data.txt` file (you have to create such file).
+
+You may also run the script with test data provided in `data.test.txt`. In order to do that, rename it to `data.txt` or change line 6 of the script to `.readFileSync(path.join(__dirname, 'data.test.txt'), 'utf8')`.
+
+Then, open the terminal in current directory context and run the script
+
+```
+node script
+```
---
+++ day01/script.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+const path = require("path");
+const performance = require("perf_hooks").performance;
+
+/** DATA LOAD */
+const data = fs
+  .readFileSync(path.join(__dirname, "data.txt"), "utf8")
+  .split("\r\n");
+
+const sumNumbersFromFirstAndLastDigits = (prev, curr) =>
+  prev + Number(`${curr[0] || 0}${curr[curr.length - 1] || 0}`);
+
+/** PART 1 */
+let t0 = performance.now();
+
+let resultOne = data
+  .map((line) =>
+    line
+      .split("")
+      .filter((c) => !isNaN(Number(c)))
+      .map((c) => Number(c))
+  )
+  .reduce(sumNumbersFromFirstAndLastDigits, 0);
+
+let t1 = performance.now() - t0;
+
+const numMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+/** PART 2 */
+t0 = performance.now();
+
+let resultTwo = 0;
+const resArray = [];
+
+data.map((line) => line.split("")).forEach((ca) => {
+  const nums = [];
+  let i = 0;
+  while (i < ca.length) {
+    if (!isNaN(Number(ca[i]))) {
+      nums.push(Number(ca[i]));
+      i++;
+      continue;
+    }
+    const substr = ca.slice(i, i + 5).join("");
+    for (let x in numMap) if (substr.startsWith(x)) nums.push(numMap[x]);
+    i++;
+  }
+  resArray.push(nums);
+});
+
+resultTwo = resArray.reduce(sumNumbersFromFirstAndLastDigits, 0);
+
+let t2 = performance.now() - t0;
+
+console.log("=============================================");
+console.log("Part 1 result:", resultOne);
+console.log(`Execution time: ${t1.toFixed(3)} ms`);
+console.log("=============================================");
+console.log("Part 2 result:", resultTwo);
+console.log(`Execution time: ${t2.toFixed(3)} ms`);
+console.log("=============================================");
